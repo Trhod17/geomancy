7bcf3afce1b343b0f21f4b2d073fdccc79dfd1fe
LenaCupidGems <lenacupid@gmail.com>
2022-01-22 15:34:37 +0100

    fixed infusions, added plate recipes

--- src/datagen/main/java/net/codersdownunder/gemmod/common/recipes/GeomancyInfusionRecipeProvider.java
+++ src/datagen/main/java/net/codersdownunder/gemmod/common/recipes/GeomancyInfusionRecipeProvider.java
@@ -61,74 +61,77 @@ public class GeomancyInfusionRecipeProvider extends RecipeProvider {
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_FUEL_TIME.get())
-		.requires(ItemInit.CITRINE.get())
-		.requires(ItemInit.CITRINE.get())
+		.requires(ItemInit.RHODONITE_DREAMING.get())
+		.requires(ItemInit.CITRINE_DREAMING.get())
 		.requires(ItemInit.SPHENE.get())
 		.requires(ItemInit.SPHENE.get())
 		.requires(ItemInit.GARNET.get())
 		.requires(ItemInit.GARNET.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_FUEL_TIME.get())
 		.group(GemMod.MODID)
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_FUEL_COAL.get())
-		.requires(ItemInit.JASPER.get())
-		.requires(ItemInit.JASPER.get())
-		.requires(ItemInit.JASPER.get())
-		.requires(ItemInit.RUBY.get())
+		.requires(ItemInit.JASPER_DREAMING.get())
 		.requires(ItemInit.RUBY.get())
 		.requires(ItemInit.RUBY.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_FUEL_COAL.get())
 		.group(GemMod.MODID)
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_SPEED_UP.get())
-		.requires(ItemInit.TOPAZ.get())
-		.requires(ItemInit.TOPAZ.get())
-		.requires(ItemInit.TOPAZ.get())
-		.requires(ItemInit.CITRINE.get())
-		.requires(ItemInit.CITRINE.get())
-		.requires(ItemInit.JASPER.get())
+		.requires(ItemInit.TOPAZ_DREAMING.get())
+		.requires(ItemInit.RUBY_DREAMING.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_SPEED_UP.get())
 		.group(GemMod.MODID)
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_SPEED_OVERDRIVE.get())
 		.requires(ItemInit.TOPAZ.get())
 		.requires(ItemInit.TOPAZ.get())
-		.requires(ItemInit.TOPAZ.get())
+		.requires(ItemInit.TOPAZ_DREAMING.get())
 		.requires(ItemInit.MALACHITE.get())
 		.requires(ItemInit.MALACHITE.get())
-		.requires(ItemInit.MALACHITE.get())
+		.requires(ItemInit.MALACHITE_DREAMING.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_SPEED_OVERDRIVE.get())
 		.group(GemMod.MODID)
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_YIELD_ORE.get())
 		.requires(ItemInit.RHODONITE.get())
-		.requires(ItemInit.RHODONITE.get())
+		.requires(ItemInit.ONYX_DREAMING.get())
 		.requires(ItemInit.CHRYSOCOLLA.get())
 		.requires(ItemInit.CHRYSOCOLLA.get())
 		.requires(ItemInit.ONYX.get())
 		.requires(ItemInit.ONYX.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_YIELD_ORE.get())
 		.group(GemMod.MODID)
 		.save(consumer);
 		
 		InfusionRecipeBuilder.infusing(ItemInit.PLATE_FAILSAFE.get())
-		.requires(ItemInit.PERIDOT.get())
+		.requires(ItemInit.PERIDOT_DREAMING.get())
 		.requires(ItemInit.PERIDOT.get())
 		.requires(ItemInit.PERIDOT.get())
 		.requires(ItemInit.PERIDOT.get())
 		.requires(ItemInit.LILYSTAR.get())
 		.requires(ItemInit.AMETHYST.get())
 		// Item below is the item to be infused
-		.requires(Items.QUARTZ_BLOCK)
+		.requires(ItemInit.PLATE_FAILSAFE.get())
+		.group(GemMod.MODID)
+		.save(consumer);
+		
+		InfusionRecipeBuilder.infusing(Items.BUDDING_AMETHYST)
+		.requires(ItemInit.AMETHYST.get())
+		.requires(ItemInit.JADE.get())
+		.requires(ItemInit.CITRINE.get())
+		.requires(ItemInit.RHODONITE.get())
+		// Item below is the item to be infused
+		.requires(Items.AMETHYST_BLOCK)
 		.group(GemMod.MODID)
 		.save(consumer);
 		
