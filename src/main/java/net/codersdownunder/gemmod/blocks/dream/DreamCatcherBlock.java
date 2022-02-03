7ede8457f6c16376d78eabb39f1d013354913391
Thomas <hedgehog.thomas@gmail.com>
2022-02-03 15:52:11 +1000

    more progress on patchouli book

--- src/main/java/net/codersdownunder/gemmod/blocks/dream/DreamCatcherBlock.java
+++ src/main/java/net/codersdownunder/gemmod/blocks/dream/DreamCatcherBlock.java
@@ -1,12 +1,9 @@
 package net.codersdownunder.gemmod.blocks.dream;
 
-import java.util.EnumMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import net.codersdownunder.gemmod.GemMod;
-import net.codersdownunder.gemmod.utils.BlockUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
@@ -24,7 +21,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -32,6 +28,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
@@ -42,61 +39,195 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.network.NetworkHooks;
 
-public class DreamCatcherBlock extends HorizontalDirectionalBlock implements SimpleWaterloggedBlock, EntityBlock {
+public class DreamCatcherBlock extends Block implements SimpleWaterloggedBlock, EntityBlock {
 
-	
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-	private static final Map<Direction, VoxelShape> SHAPES = new EnumMap<>(Direction.class);
 	
 	private static final Optional<VoxelShape> SHAPE = Optional.of(Stream.of(
 			Block.box(12.26329, -8.75, 7.01329, 13.51329, -5.5, 8.01329),
 			Block.box(7.51329, 13, 7.51329, 8.51329, 16, 8.51329),
 			Block.box(12.51329, -4, 7.51329, 13.51329, 2, 8.51329),
-			Block.box(2.5132899999999996, -3, 7.51329, 3.5132899999999996, 2, 8.51329),
+			Block.box(2.51329, -3, 7.51329, 3.51329, 2, 8.51329),
 			Block.box(7.51329, -4, 7.51329, 8.51329, 0, 8.51329),
 			Block.box(7.51329, -16, 7.51329, 8.51329, -10, 8.51329),
 			Block.box(10.01329, -15, 7.51329, 11.01329, -9, 8.51329),
 			Block.box(5.01329, -14, 7.51329, 6.01329, -9, 8.51329),
 			Block.box(13.01329, 4, 7.01329, 15.01329, 9, 9.01329),
-			Block.box(1.0132899999999996, 4, 7.01329, 3.0132899999999996, 9, 9.01329),
+			Block.box(1.01329, 4, 7.01329, 3.01329, 9, 9.01329),
 			Block.box(4.01329, 11, 7.01329, 12.01329, 13, 9.01329),
-			Block.box(2.0132899999999996, 9, 7.01329, 4.01329, 11, 9.01329),
+			Block.box(2.01329, 9, 7.01329, 4.01329, 11, 9.01329),
 			Block.box(12.01329, 9, 7.01329, 14.01329, 11, 9.01329),
-			Block.box(2.0132899999999996, 2, 7.01329, 4.01329, 4, 9.01329),
+			Block.box(2.01329, 2, 7.01329, 4.01329, 4, 9.01329),
 			Block.box(12.01329, 2, 7.01329, 14.01329, 4, 9.01329),
 			Block.box(4.01329, 0, 7.01329, 12.01329, 2, 9.01329),
 			Block.box(6.01329, -10, 7.51329, 10.01329, -9, 8.51329),
 			Block.box(10.01329, -9, 7.51329, 11.01329, -5, 8.51329),
 			Block.box(5.01329, -9, 7.51329, 6.01329, -5, 8.51329),
 			Block.box(6.01329, -5, 7.51329, 10.01329, -4, 8.51329),
-			Block.box(6.3976363255372775, 0.7833697771617576, 7.76329, 7.3976363255372775, 11.783369777161758, 7.76329),
-			Block.box(8.626920392436872, 0.7731980515294925, 8.01329, 9.626920392436872, 11.773198051529493, 8.01329),
-			Block.box(3.6269203924368707, 5.773198051529493, 8.26329, 14.62692039243687, 6.773198051529493, 8.26329),
-			Block.box(1.3976363255372775, 5.783369777161757, 8.51329, 12.397636325537277, 6.783369777161757, 8.51329),
-			Block.box(12.64219568298503, -4.286100784741091, 8.01329, 18.64219568298503, -3.286100784741091, 8.01329),
-			Block.box(14.89219568298503, -7.036100784741091, 8.26329, 15.89219568298503, -1.036100784741091, 8.26329),
+			Block.box(6.39764, 0.78337, 7.76329, 7.39764, 11.78337, 7.76329),
+			Block.box(8.62692, 0.7732, 8.01329, 9.62692, 11.7732, 8.01329),
+			Block.box(3.62692, 5.7732, 8.26329, 14.62692, 6.7732, 8.26329),
+			Block.box(1.39764, 5.78337, 8.51329, 12.39764, 6.78337, 8.51329),
+			Block.box(12.6422, -4.2861, 8.01329, 18.6422, -3.2861, 8.01329),
+			Block.box(14.8922, -7.0361, 8.26329, 15.8922, -1.0361, 8.26329),
 			Block.box(12.26329, -1, 7.26329, 13.76329, 0.5, 8.76329),
 			Block.box(12.26329, -2.75, 7.26329, 13.76329, -1.25, 8.76329),
-			Block.box(2.2632899999999996, -0.5, 7.26329, 3.7632899999999996, 1, 8.76329),
+			Block.box(2.26329, -0.5, 7.26329, 3.76329, 1, 8.76329),
 			Block.box(7.01329, 5.5, 7.01329, 9.01329, 7.5, 9.01329),
 			Block.box(7.26329, -7.75, 7.26329, 8.76329, -6.25, 8.76329),
 			Block.box(12.26329, -5.5, 7.26329, 13.76329, -4, 8.76329),
-			Block.box(2.2632899999999996, -4.5, 7.26329, 3.7632899999999996, -3, 8.76329),
-			Block.box(4.8959576213188285, 5.612069791076927, 7.51329, 6.3959576213188285, 7.112069791076927, 9.01329),
-			Block.box(6.201804086868384, 8.627441328665881, 7.01329, 7.701804086868384, 10.127441328665881, 8.51329),
-			Block.box(8.423879532511286, 2.4546165676349103, 7.26329, 9.923879532511286, 3.9546165676349103, 8.76329),
+			Block.box(2.26329, -4.5, 7.26329, 3.76329, -3, 8.76329),
+			Block.box(4.89596, 5.61207, 7.51329, 6.39596, 7.11207, 9.01329),
+			Block.box(6.2018, 8.62744, 7.01329, 7.7018, 10.12744, 8.51329),
+			Block.box(8.42388, 2.45462, 7.26329, 9.92388, 3.95462, 8.76329),
 			Block.box(4.76329, -13, 7.26329, 6.26329, -11.5, 8.76329),
 			Block.box(7.26329, -12.5, 7.26329, 8.76329, -11, 8.76329),
 			Block.box(9.76329, -13.5, 7.26329, 11.26329, -12, 8.76329),
-			Block.box(2.0132899999999996, -7, 8.01329, 3.2632899999999996, -4.5, 9.01329),
-			Block.box(2.7632899999999996, -7.75, 7.01329, 4.01329, -4.5, 8.01329),
+			Block.box(2.01329, -7, 8.01329, 3.26329, -4.5, 9.01329),
+			Block.box(2.76329, -7.75, 7.01329, 4.01329, -4.5, 8.01329),
 			Block.box(12.51329, -8, 8.01329, 13.76329, -5.5, 9.01329)
+			).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get());
+	
+	private static final Optional<VoxelShape> SHAPE_SOUTH = Optional.of(Stream.of(
+			Block.box(2.4867100000000004, -8.75, 8.01329, 3.7367100000000004, -5.5, 9.01329),
+			Block.box(7.48671, 13, 7.51329, 8.48671, 16, 8.51329),
+			Block.box(2.4867100000000004, -4, 7.51329, 3.4867100000000004, 2, 8.51329),
+			Block.box(12.48671, -3, 7.51329, 13.48671, 2, 8.51329),
+			Block.box(7.48671, -4, 7.51329, 8.48671, 0, 8.51329),
+			Block.box(7.48671, -16, 7.51329, 8.48671, -10, 8.51329),
+			Block.box(4.98671, -15, 7.51329, 5.98671, -9, 8.51329),
+			Block.box(9.98671, -14, 7.51329, 10.98671, -9, 8.51329),
+			Block.box(0.9867100000000004, 4, 7.01329, 2.9867100000000004, 9, 9.01329),
+			Block.box(12.98671, 4, 7.01329, 14.98671, 9, 9.01329),
+			Block.box(3.9867100000000004, 11, 7.01329, 11.98671, 13, 9.01329),
+			Block.box(11.98671, 9, 7.01329, 13.98671, 11, 9.01329),
+			Block.box(1.9867100000000004, 9, 7.01329, 3.9867100000000004, 11, 9.01329),
+			Block.box(11.98671, 2, 7.01329, 13.98671, 4, 9.01329),
+			Block.box(1.9867100000000004, 2, 7.01329, 3.9867100000000004, 4, 9.01329),
+			Block.box(3.9867100000000004, 0, 7.01329, 11.98671, 2, 9.01329),
+			Block.box(5.98671, -10, 7.51329, 9.98671, -9, 8.51329),
+			Block.box(4.98671, -9, 7.51329, 5.98671, -5, 8.51329),
+			Block.box(9.98671, -9, 7.51329, 10.98671, -5, 8.51329),
+			Block.box(5.98671, -5, 7.51329, 9.98671, -4, 8.51329),
+			Block.box(8.602360000000001, 0.78337, 8.26329, 9.602360000000001, 11.78337, 8.26329),
+			Block.box(6.37308, 0.7732, 8.01329, 7.37308, 11.7732, 8.01329),
+			Block.box(1.3730799999999999, 5.7732, 7.76329, 12.37308, 6.7732, 7.76329),
+			Block.box(3.602359999999999, 5.78337, 7.51329, 14.602360000000001, 6.78337, 7.51329),
+			Block.box(-2.6422000000000008, -4.2861, 8.01329, 3.3577999999999992, -3.2861, 8.01329),
+			Block.box(0.10779999999999923, -7.0361, 7.76329, 1.1077999999999992, -1.0361, 7.76329),
+			Block.box(2.2367100000000004, -1, 7.26329, 3.7367100000000004, 0.5, 8.76329),
+			Block.box(2.2367100000000004, -2.75, 7.26329, 3.7367100000000004, -1.25, 8.76329),
+			Block.box(12.23671, -0.5, 7.26329, 13.73671, 1, 8.76329),
+			Block.box(6.98671, 5.5, 7.01329, 8.98671, 7.5, 9.01329),
+			Block.box(7.23671, -7.75, 7.26329, 8.73671, -6.25, 8.76329),
+			Block.box(2.2367100000000004, -5.5, 7.26329, 3.7367100000000004, -4, 8.76329),
+			Block.box(12.23671, -4.5, 7.26329, 13.73671, -3, 8.76329),
+			Block.box(9.604040000000001, 5.61207, 7.01329, 11.104040000000001, 7.11207, 8.51329),
+			Block.box(8.2982, 8.62744, 7.51329, 9.7982, 10.12744, 9.01329),
+			Block.box(6.0761199999999995, 2.45462, 7.26329, 7.5761199999999995, 3.95462, 8.76329),
+			Block.box(9.73671, -13, 7.26329, 11.23671, -11.5, 8.76329),
+			Block.box(7.23671, -12.5, 7.26329, 8.73671, -11, 8.76329),
+			Block.box(4.73671, -13.5, 7.26329, 6.23671, -12, 8.76329),
+			Block.box(12.73671, -7, 7.01329, 13.98671, -4.5, 8.01329),
+			Block.box(11.98671, -7.75, 8.01329, 13.23671, -4.5, 9.01329),
+			Block.box(2.2367100000000004, -8, 7.01329, 3.4867100000000004, -5.5, 8.01329)
+			).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get());
+	
+	private static final Optional<VoxelShape> SHAPE_WEST = Optional.of(Stream.of(
+			Block.box(7, -8.75, 2.5, 8, -5.5, 3.75),
+			Block.box(7.5, 13, 7.5, 8.5, 16, 8.5),
+			Block.box(7.5, -4, 2.5, 8.5, 2, 3.5),
+			Block.box(7.5, -3, 12.5, 8.5, 2, 13.5),
+			Block.box(7.5, -4, 7.5, 8.5, 0, 8.5),
+			Block.box(7.5, -16, 7.5, 8.5, -10, 8.5),
+			Block.box(7.5, -15, 5, 8.5, -9, 6),
+			Block.box(7.5, -14, 10, 8.5, -9, 11),
+			Block.box(7, 4, 1, 9, 9, 3),
+			Block.box(7, 4, 13, 9, 9, 15),
+			Block.box(7, 11, 4, 9, 13, 12),
+			Block.box(7, 9, 12, 9, 11, 14),
+			Block.box(7, 9, 2, 9, 11, 4),
+			Block.box(7, 2, 12, 9, 4, 14),
+			Block.box(7, 2, 2, 9, 4, 4),
+			Block.box(7, 0, 4, 9, 2, 12),
+			Block.box(7.5, -10, 6, 8.5, -9, 10),
+			Block.box(7.5, -9, 5, 8.5, -5, 6),
+			Block.box(7.5, -9, 10, 8.5, -5, 11),
+			Block.box(7.5, -5, 6, 8.5, -4, 10),
+			Block.box(7.75, 0.78337, 8.61565, 7.75, 11.78337, 9.61565),
+			Block.box(8, 0.7732, 6.386369999999999, 8, 11.7732, 7.386369999999999),
+			Block.box(8.25, 5.7732, 1.3863699999999994, 8.25, 6.7732, 12.38637),
+			Block.box(8.5, 5.78337, 3.6156499999999987, 8.5, 6.78337, 14.61565),
+			Block.box(8, -4.2861, -2.628910000000001, 8, -3.2861, 3.371089999999999),
+			Block.box(8.25, -7.0361, 0.12108999999999881, 8.25, -1.0361, 1.1210899999999988),
+			Block.box(7.25, -1, 2.25, 8.75, 0.5, 3.75),
+			Block.box(7.25, -2.75, 2.25, 8.75, -1.25, 3.75),
+			Block.box(7.25, -0.5, 12.25, 8.75, 1, 13.75),
+			Block.box(7, 5.5, 7, 9, 7.5, 9),
+			Block.box(7.25, -7.75, 7.25, 8.75, -6.25, 8.75),
+			Block.box(7.25, -5.5, 2.25, 8.75, -4, 3.75),
+			Block.box(7.25, -4.5, 12.25, 8.75, -3, 13.75),
+			Block.box(7.5, 5.61207, 9.61733, 9, 7.11207, 11.11733),
+			Block.box(7, 8.62744, 8.31149, 8.5, 10.12744, 9.81149),
+			Block.box(7.25, 2.45462, 6.089409999999999, 8.75, 3.95462, 7.589409999999999),
+			Block.box(7.25, -13, 9.75, 8.75, -11.5, 11.25),
+			Block.box(7.25, -12.5, 7.25, 8.75, -11, 8.75),
+			Block.box(7.25, -13.5, 4.75, 8.75, -12, 6.25),
+			Block.box(8, -7, 12.75, 9, -4.5, 14),
+			Block.box(7, -7.75, 12, 8, -4.5, 13.25),
+			Block.box(8, -8, 2.25, 9, -5.5, 3.5)
+			).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get());
+		
+	private static final Optional<VoxelShape> SHAPE_EAST = Optional.of(Stream.of(
+			Block.box(8, -8.75, 12.27658, 9, -5.5, 13.52658),
+			Block.box(7.5, 13, 7.526579999999999, 8.5, 16, 8.52658),
+			Block.box(7.5, -4, 12.52658, 8.5, 2, 13.52658),
+			Block.box(7.5, -3, 2.526579999999999, 8.5, 2, 3.526579999999999),
+			Block.box(7.5, -4, 7.526579999999999, 8.5, 0, 8.52658),
+			Block.box(7.5, -16, 7.526579999999999, 8.5, -10, 8.52658),
+			Block.box(7.5, -15, 10.02658, 8.5, -9, 11.02658),
+			Block.box(7.5, -14, 5.026579999999999, 8.5, -9, 6.026579999999999),
+			Block.box(7, 4, 13.02658, 9, 9, 15.02658),
+			Block.box(7, 4, 1.0265799999999992, 9, 9, 3.026579999999999),
+			Block.box(7, 11, 4.026579999999999, 9, 13, 12.02658),
+			Block.box(7, 9, 2.026579999999999, 9, 11, 4.026579999999999),
+			Block.box(7, 9, 12.02658, 9, 11, 14.02658),
+			Block.box(7, 2, 2.026579999999999, 9, 4, 4.026579999999999),
+			Block.box(7, 2, 12.02658, 9, 4, 14.02658),
+			Block.box(7, 0, 4.026579999999999, 9, 2, 12.02658),
+			Block.box(7.5, -10, 6.026579999999999, 8.5, -9, 10.02658),
+			Block.box(7.5, -9, 10.02658, 8.5, -5, 11.02658),
+			Block.box(7.5, -9, 5.026579999999999, 8.5, -5, 6.026579999999999),
+			Block.box(7.5, -5, 6.026579999999999, 8.5, -4, 10.02658),
+			Block.box(8.25, 0.78337, 6.41093, 8.25, 11.78337, 7.41093),
+			Block.box(8, 0.7732, 8.64021, 8, 11.7732, 9.64021),
+			Block.box(7.75, 5.7732, 3.6402099999999997, 7.75, 6.7732, 14.64021),
+			Block.box(7.5, 5.78337, 1.4109299999999996, 7.5, 6.78337, 12.41093),
+			Block.box(8, -4.2861, 12.65549, 8, -3.2861, 18.65549),
+			Block.box(7.75, -7.0361, 14.90549, 7.75, -1.0361, 15.90549),
+			Block.box(7.25, -1, 12.27658, 8.75, 0.5, 13.77658),
+			Block.box(7.25, -2.75, 12.27658, 8.75, -1.25, 13.77658),
+			Block.box(7.25, -0.5, 2.276579999999999, 8.75, 1, 3.776579999999999),
+			Block.box(7, 5.5, 7.026579999999999, 9, 7.5, 9.02658),
+			Block.box(7.25, -7.75, 7.276579999999999, 8.75, -6.25, 8.77658),
+			Block.box(7.25, -5.5, 12.27658, 8.75, -4, 13.77658),
+			Block.box(7.25, -4.5, 2.276579999999999, 8.75, -3, 3.776579999999999),
+			Block.box(7, 5.61207, 4.909249999999999, 8.5, 7.11207, 6.409249999999999),
+			Block.box(7.5, 8.62744, 6.21509, 9, 10.12744, 7.71509),
+			Block.box(7.25, 2.45462, 8.43717, 8.75, 3.95462, 9.93717),
+			Block.box(7.25, -13, 4.776579999999999, 8.75, -11.5, 6.276579999999999),
+			Block.box(7.25, -12.5, 7.276579999999999, 8.75, -11, 8.77658),
+			Block.box(7.25, -13.5, 9.77658, 8.75, -12, 11.27658),
+			Block.box(7, -7, 2.026579999999999, 8, -4.5, 3.276579999999999),
+			Block.box(8, -7.75, 2.776579999999999, 9, -4.5, 4.026579999999999),
+			Block.box(7, -8, 12.52658, 8, -5.5, 13.77658)
 			).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get());
 	
 	public DreamCatcherBlock(Properties p_49795_) {
 		super(p_49795_);
 		registerDefaultState(this.stateDefinition.any().setValue(BlockStateProperties.WATERLOGGED, false).setValue(FACING, Direction.NORTH));
-		runCalculation(SHAPE.orElse(Shapes.block()));
+	
 	}
 	
 	@Override
@@ -120,25 +251,37 @@ public class DreamCatcherBlock extends HorizontalDirectionalBlock implements Sim
 
     
     @Override
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext)
-    {
-        return SHAPES.get(pState.getValue(FACING));
-    }
-    
-    @Override
-    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos,
-    		CollisionContext pContext) {
-    	return SHAPE.get();
-    }
-    
-    protected void runCalculation(VoxelShape shape) {
-		for (Direction direction : Direction.values())
-			SHAPES.put(direction, BlockUtils.calculateShapes(direction, shape));
+	public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+		switch(pState.getValue(FACING)) {
+		case EAST:
+			return SHAPE_EAST.get();
+		case WEST:
+			return SHAPE_WEST.get();
+		case SOUTH:
+			return SHAPE_SOUTH.get();
+		default:
+			return SHAPE.get();
+		}
 	}
-    
-    public RenderShape getRenderShape(BlockState pState) {
-        return RenderShape.MODEL;
-     }
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos,
+			CollisionContext pContext) {
+		switch(pState.getValue(FACING)) {
+		case EAST:
+			return SHAPE_EAST.get();
+		case WEST:
+			return SHAPE_WEST.get();
+		case SOUTH:
+			return SHAPE_SOUTH.get();
+		default:
+			return SHAPE.get();
+		}
+	}
+
+	public RenderShape getRenderShape(BlockState pState) {
+		return RenderShape.MODEL;
+	}
     
     @SuppressWarnings("deprecation")
     @Override
